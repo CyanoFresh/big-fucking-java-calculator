2926fba8302bff94fe5a7a1c2b731caf161679ba
CyanoFresh <cyanofresh@gmail.com>
2017-10-21 00:50:59 +0300

    Fixes

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("My fucking calculator bullshit");
+        primaryStage.setTitle("Big fucking java calculator");
         primaryStage.setScene(new Scene(root, 300, 400));
         primaryStage.show();
     }
